d560f10b45682dbfdc422b637fcc07c853250ded
soniaobasuyi <obasuyisonia@gmail.com>
2024-09-04 14:09:53 +0100

    initial commit + header section added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
+import Header from "./components/Header.jsx";
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path={'/'} element={<Home />} />
+                <Route path={'/sign-in'} element={<SignIn />} />
+                <Route path={'/sign-up'} element={<SignUp />} />
+                <Route path={'/about'} element={<About />} />
+                <Route path={'/profile'} element={<Profile />} />
+
+            </Routes>
+        </BrowserRouter>
+
+    )
+}
