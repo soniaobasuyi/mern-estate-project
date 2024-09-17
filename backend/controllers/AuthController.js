f261a2e2aac7249f7b93bc2a162df70bfd907e10
soniaobasuyi <obasuyisonia@gmail.com>
2024-09-17 15:14:40 +0100

    sign-in api route created

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,5 +1,8 @@
 import User from "../models/UserModel.js";
 import bcryptjs from 'bcryptjs';
+import {errorHandler} from "../utils/Error.js";
+import jwt from 'jsonwebtoken'
+import {now} from "mongoose";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -12,5 +15,23 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-
 };
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(404, 'Wrong credentials!'));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: userPassword, ...userInfoWithoutPassword } = validUser._doc;
+        res
+            .cookie('access-token', token, { httpOnly: true, expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) })
+            .status(200)
+            .json(userInfoWithoutPassword);
+    } catch (error) {
+        next(error);
+    }
+}
