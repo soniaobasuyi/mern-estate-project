ebcbdb471f36a60ab347dfdeedddd98f3aae378f
soniaobasuyi <obasuyisonia@gmail.com>
2024-09-30 19:03:36 +0100

    redux persist added

--- frontend/src/redux/user/userSlice.js
+++ frontend/src/redux/user/userSlice.js
@@ -26,4 +26,4 @@ const userSlice = createSlice({
 });
 
 export const {signInStart, signInSuccess, signInFailure} = userSlice.actions;
-export const userReducer = userSlice.reducer;
+export default userSlice.reducer;
