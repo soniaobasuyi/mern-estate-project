65356c4b432b55a48d43a1278b3b3aabd69966f3
soniaobasuyi <obasuyisonia@gmail.com>
2025-02-22 00:31:17 +0100

    added show listings functionality in the profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -21,6 +21,8 @@ export default function Profile() {
     const [fileUploadError, setFileUploadError] = useState(false);
     const [formData, setFormData] = useState({});
     const [updateSuccess, setUpdateSuccess] = useState(false);
+    const [showListingsError, setShowListingsError] = useState(false);
+    const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -54,7 +56,7 @@ export default function Profile() {
     const handleChange = (e) => {
         setFormData({...formData, [e.target.id]: e.target.value});
     };
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
@@ -110,6 +112,21 @@ export default function Profile() {
         }
     };
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`api/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+            setUserListings(data);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+
     return (
         <div className={'p-3 max-w-lg mx-auto'}>
             <h1 className={'text-3xl font-semibold text-center my-7 text-slate-700'}>Profile</h1>
@@ -130,12 +147,12 @@ export default function Profile() {
                 <p className={'text-sm self-center'}>
                     {
                         fileUploadError ?
-                        (<span className={'text-red-700'}>Image upload error(image must be less than 2MB)</span>) :
-                        filePercentage > 0 && filePercentage < 100 ?
-                        (<span className={'text-slate-700'}>{`Uploading ${filePercentage}%`}</span>) :
-                        filePercentage === 100 ?
-                        (<span className={'text-green-700'}>Image successfully uploaded!</span>) :
-                        ""
+                            (<span className={'text-red-700'}>Image upload error(image must be less than 2MB)</span>) :
+                            filePercentage > 0 && filePercentage < 100 ?
+                                (<span className={'text-slate-700'}>{`Uploading ${filePercentage}%`}</span>) :
+                                filePercentage === 100 ?
+                                    (<span className={'text-green-700'}>Image successfully uploaded!</span>) :
+                                    ""
                     }
                 </p>
                 <input
@@ -161,10 +178,12 @@ export default function Profile() {
                     className={'border p-3 rounded-lg'}
                     onChange={handleChange}
                 />
-                <button disabled={loading} className={'bg-slate-700 text-white uppercase p-3 rounded-lg hover:opacity-95 disabled:opacity-80'}>
+                <button disabled={loading}
+                        className={'bg-slate-700 text-white uppercase p-3 rounded-lg hover:opacity-95 disabled:opacity-80'}>
                     {loading ? 'Loading...' : 'Update'}
                 </button>
-                <Link className={'bg-green-700 text-white p-3 rounded-lg text-center uppercase hover:opacity-95'} to={'/create-listing'}>
+                <Link className={'bg-green-700 text-white p-3 rounded-lg text-center uppercase hover:opacity-95'}
+                      to={'/create-listing'}>
                     Create Listing
                 </Link>
 
@@ -175,6 +194,30 @@ export default function Profile() {
             </div>
             <p className={'text-red-700 mt-5'}>{error ? error : ''}</p>
             <p className={'text-green-700 mt-5'}>{updateSuccess ? 'User successfully updated!' : ''}</p>
+            <button onClick={handleShowListings} className={'text-green-700 w-full hover:opacity-85'}>Show listings</button>
+            <p className={'text-red-700 mt-5'}>{showListingsError ? 'Error showing listings' : ''}</p>
+
+            {
+                userListings && userListings.length > 0 &&
+                <div className={'flex flex-col gap-4'}>
+                    <h1 className={'text-center mt-7 text-xl font-semibold text-slate-700'}>Your Listings</h1>
+                    {userListings.map((listing) =>
+                        <div key={listing._id} className={'border rounded-lg p-3 flex justify-between items-center gap-4'}>
+                            <Link to={`listing/${listing._id}`}>
+                                <img src={listing.imageURLs[0]} alt={'listing image'} className={'h-16 w-16 object-contain'}/>
+                            </Link>
+                            <Link to={`listing/${listing._id}`} className={'text-slate-700 font-semibold hover:underline truncate flex-1'}>
+                                <p>{listing.title}</p>
+                            </Link>
+
+                            <div className={'flex flex-col'}>
+                                <button className={'text-red-700 uppercase'}>Delete</button>
+                                <button className={'text-green-700 uppercase'}>Edit</button>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            }
         </div>
-    )
+    );
 }
