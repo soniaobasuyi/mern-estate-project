779b9576301700b853acdf28a59b150339b143b7
soniaobasuyi <obasuyisonia@gmail.com>
2025-02-22 13:14:30 +0100

    completed the delete listing functionality

--- backend/routes/ListingRoute.js
+++ backend/routes/ListingRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createListing} from "../controllers/ListingController.js";
+import {createListing, deleteListing} from "../controllers/ListingController.js";
 import {verifyUser} from "../utils/VerifyUser.js";
 
 const listingRouter = express.Router();
 
 listingRouter.post('/create', verifyUser, createListing);
+listingRouter.delete('/delete/:id', verifyUser, deleteListing);
 
 export default listingRouter;
