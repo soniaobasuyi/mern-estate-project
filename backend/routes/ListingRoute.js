31f30ec9c8e9667b1e9e80d5aa0e72b82ab65830
soniaobasuyi <obasuyisonia@gmail.com>
2025-02-22 13:35:38 +0100

    created api route for updating a listing

--- backend/routes/ListingRoute.js
+++ backend/routes/ListingRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createListing, deleteListing} from "../controllers/ListingController.js";
+import {createListing, deleteListing, updateListing} from "../controllers/ListingController.js";
 import {verifyUser} from "../utils/VerifyUser.js";
 
 const listingRouter = express.Router();
 
 listingRouter.post('/create', verifyUser, createListing);
 listingRouter.delete('/delete/:id', verifyUser, deleteListing);
+listingRouter.post('/update/:id', verifyUser, updateListing);
 
 export default listingRouter;
