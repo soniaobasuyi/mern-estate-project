b7bf62398af1cd5065f15ba0ecd80f1e6aa9add3
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-14 22:08:48 +0100

    created search api route

--- backend/routes/ListingRoute.js
+++ backend/routes/ListingRoute.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import {createListing, deleteListing, getListing, updateListing} from "../controllers/ListingController.js";
+import {
+    createListing,
+    deleteListing,
+    getListing,
+    searchListings,
+    updateListing
+} from "../controllers/ListingController.js";
 import {verifyUser} from "../utils/VerifyUser.js";
 
 const listingRouter = express.Router();
@@ -8,5 +14,6 @@ listingRouter.post('/create', verifyUser, createListing);
 listingRouter.delete('/delete/:id', verifyUser, deleteListing);
 listingRouter.post('/update/:id', verifyUser, updateListing);
 listingRouter.get('/getListing/:id', getListing);
+listingRouter.get('/get', searchListings);
 
 export default listingRouter;
