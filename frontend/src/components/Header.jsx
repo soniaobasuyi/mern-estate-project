eab9a120115d7b62f0a6eac26567e9ceeba0f296
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-26 17:52:44 +0100

    minor refactor

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
         if (searchTermFromUrl) {
             setSearchTerm(searchTermFromUrl);
         }
-    }, []);
+    }, [location.search]);
     
 
     return (
