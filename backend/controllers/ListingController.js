6c030b910b3ed2ed6a88439920ca5188fbc77852
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-27 13:23:07 +0100

    minor refactor

--- backend/controllers/ListingController.js
+++ backend/controllers/ListingController.js
@@ -59,34 +59,29 @@ export const searchListings = async (req, res, next) => {
     try {
         const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
-        let offer = req.query.offer;
 
+        let offer = req.query.offer;
         if (offer === undefined || offer === 'false') {
             offer = { $in: [false, true] };
         }
 
         let parking = req.query.parking;
-
         if (parking === undefined || parking === 'false') {
             parking = { $in: [false, true] };
         }
 
         let furnished = req.query.furnished;
-
         if (furnished === undefined || furnished === 'false') {
             furnished = { $in: [false, true] };
         }
 
         let type = req.query.type;
-
         if (type === undefined || type === 'all') {
             type = { $in: ['sale', 'rent'] };
         }
 
         const searchTerm = req.query.searchTerm || '';
-
         const sort = req.query.sort || 'createdAt';
-
         const order = req.query.order || 'desc';
 
         const listings = await Listing.find({
@@ -95,13 +90,9 @@ export const searchListings = async (req, res, next) => {
             furnished,
             parking,
             type
-        }).sort(
-            {[sort]: order}
-        ).limit(limit).skip(startIndex);
+        }).sort({[sort]: order}).limit(limit).skip(startIndex);
 
         return res.status(200).json(listings);
-
-
     } catch (error) {
         next(error);
     }
