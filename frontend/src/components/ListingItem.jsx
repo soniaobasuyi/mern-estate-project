5c2597febd57bdf525680a4412dd3f33f04ab143
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-28 15:32:54 +0100

    style refactor

--- frontend/src/components/ListingItem.jsx
+++ frontend/src/components/ListingItem.jsx
@@ -4,7 +4,7 @@ import {MdLocationOn} from "react-icons/md";
 
 export default function ListingItem( {listing} ) {
     return (
-        <div className={'bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[400px]'}>
+        <div className={'bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'}>
             <Link to={`/listing/${listing._id}`}>
                 <img
                     alt={'cover-image'}
