948b944bf3dc5681499ecef77dafb9ac642073c2
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-29 16:54:02 +0100

    added z-index on the header to ensure nothing overlaps it

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
     
 
     return (
-        <header className={'bg-slate-200 drop-shadow-md'}>
+        <header className={'bg-slate-200 drop-shadow-md relative z-50'}>
             <div className={'flex justify-between items-center max-w-6xl mx-auto p-3'}>
                 <Link to={'/'}>
                     <h1 className={'font-bold text-sm sm:text-xl flex flex-wrap'}>
