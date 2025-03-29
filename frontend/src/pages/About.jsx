ae130b7adae622a4e12d2f30800cdf0032dd1716
soniaobasuyi <obasuyisonia@gmail.com>
2025-03-29 10:55:55 +0100

    created about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div className={'py-20 px-5 max-w-6xl mx-auto'}>
+            <h1 className={'text-slate-700 text-3xl font-bold mb-4'}>
+                About Sonia Estate
+            </h1>
+            <p className={'mb-4 text-slate-600'}>
+                Sonia Estate is a learning project, but if it were real, it would be your ultimate destination for finding the perfect home with ease and confidence. Designed to provide a seamless real estate experience, Sonia Estate would connect buyers, renters, and sellers with curated listings tailored to their needs. From modern apartments to spacious family homes, the platform would offer a user-friendly interface, smart search filters, and real-time updates to simplify the home-hunting process.
+            </p>
+            <p className={'mb-4 text-slate-600'}>
+                Built with the MERN stack (MongoDB, Express, React, and Node.js), this project serves as a hands-on opportunity to deepen my skills in full-stack development. Through Sonia Estate, I have implemented key features such as secure authentication, database management, and dynamic property listings. The project also explores responsive UI design, interactive elements, and API integrations to enhance the overall user experience.
+            </p>
+            <p className={'mb-4 text-slate-600'}>
+                While Sonia Estate may not be a live real estate platform, it reflects my dedication to learning, improving, and pushing the boundaries of web development. As I continue refining its features, the goal is to create an intuitive, scalable, and efficient application that mirrors real-world real estate solutions. This project is more than just code — it is a testament to growth, curiosity, and a passion for building meaningful digital experiences.
+            </p>
+        </div>
     )
 }
