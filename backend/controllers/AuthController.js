10c3c0882b1f9ec4800f9dbbf0baaab21876139f
soniaobasuyi <obasuyisonia@gmail.com>
2025-12-08 11:37:24 +0100

    example

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -21,7 +21,7 @@ export const signin = async (req, res, next) => {
 
     try {
         const validUser = await User.findOne({ email });
-        if (!validUser) return next(errorHandler(404, 'User not found!'));
+        if (!validUser) return next(errorHandler(404, 'update this'));
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(404, 'Wrong credentials!'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
