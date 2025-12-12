28af8a34024bc611e4ac433fa9056dcd40398cc3
soniaobasuyi <obasuyisonia@gmail.com>
2025-12-12 22:17:35 +0100

    added conditions for empty signin field

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -20,10 +20,16 @@ export const signin = async (req, res, next) => {
     const {email, password} = req.body;
 
     try {
+        if (!email || !password) {
+            return next(errorHandler(400, 'Email and password required'));
+        }
+
         const validUser = await User.findOne({ email });
         if (!validUser) return next(errorHandler(404, 'User not found!'));
+
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) return next(errorHandler(404, 'Wrong credentials!'));
+
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: userPassword, ...userInfoWithoutPassword } = validUser._doc;
         res
